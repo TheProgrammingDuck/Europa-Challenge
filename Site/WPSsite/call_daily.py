feafaefd8493720b11073feeb6d9a2033084b261
Vishal Soomaney <vsv501@york.ac.uk>
2017-08-07 06:08:54 +0200

    Add files via upload
    Added more comments

--- Site/WPSsite/call_daily.py
+++ Site/WPSsite/call_daily.py
@@ -1,21 +1,27 @@
-import datetime
 from threading import Timer
 import dark_sky as darksky
 import MakePrediction as MakePrediction
 import schedule
 import time
 
+"""The url for the csv containing forest locations and our Dark Sky key.
+Dark Sky key has been removed so that it cannot be copied and used by another"""
+
 file='forest_locations.csv'
 key = 'e5d530b79eb4c2232e41a7ab30a208e4'
 
+"""Initialising once, all scripts have been optimised so that they don't 
+have to be reinitialised to handle updated data"""
 weatherGetter = darksky.Weather_data(file,key, 210)
 makePrediction= MakePrediction.MakePrediction()
 
 def call_daily():
 
+    """Updating everything in the right order, the output of
+    "makePrediction.prediction()" is used by webworldwind"""
     weatherGetter.produce_Output("add1")
     print("1 day of data added")
-    weatherGetter.produce_final()
+    weatherGetter.produce_average()
     print("14 day averages updated")
     weatherGetter.produce_Output("future")
     print("Future forecast obtained")
@@ -24,7 +30,8 @@ def call_daily():
     makePrediction.prediction()
     print("SVM output updated/created")
 
-
+"""The script runs automatically at 1 am EST. The timezone used depends on the timezone
+of the pc or server running the scripts"""
 schedule.every().day.at("01:00").do(call_daily)
 while True:
     schedule.run_pending()
--- Site/WPSsite/dark_sky.py
+++ Site/WPSsite/dark_sky.py
@@ -18,8 +18,6 @@ class Weather_data(object):
     def csv_to_array(self, file):    
         forest_locations=(np.genfromtxt(file, delimiter=','))
         forest_locations= forest_locations.tolist()
-
-        #print(forest_locations)
         
         return(forest_locations)
     
@@ -64,13 +62,11 @@ class Weather_data(object):
                 hour_counter=0
 
                 #Checks weather data per hour, appends to an array then averages the data.
-                
                 for element in hourly_data:
-                    #print(count)
-                    count+=1
                     
-                    #location_array.append(loc)
-                    #hourly_array.append(int(element['time']))
+                    count+=1
+
+                    #Add hourly data to temporary arrays
                     temperature_array.append(element['temperature'])
                     humidity_array.append(element['humidity']*100)
                     wind_array.append(element['windSpeed'])
@@ -82,7 +78,7 @@ class Weather_data(object):
 
                     hour_counter+=1
 
-                    #if hour_counter==24:
+                #checking number of hours that contain rain. Ideally we use use mm/hour however we need to use the same standards used in the training data.  
                 if rain_counter==0:
                     pass
                 elif rain_counter<3:
@@ -93,7 +89,8 @@ class Weather_data(object):
                     rain_amount=3
                 else:
                     rain_amount=4
-                #print(sum(humidity_array)/float(len(humidity_array)))
+
+                #Once you have hourly weather data, you merge it to obtain daily averages
                 temp_list=[]
                 temp_list.append(locationCount)
                 temp_list.append(loc[0])
@@ -102,29 +99,31 @@ class Weather_data(object):
                 temp_list.append(sum(humidity_array)/float(len(humidity_array)))
                 temp_list.append(sum(wind_array)/float(len(wind_array)))
                 temp_list.append(rain_amount)
-                
+
+                #you append the daily averages to a final array
                 all_data.append(temp_list)
+
+                #emptying arrays for next location
                 location_array=[]
                 temperature_array=[]
                 humidity_array=[]
                 wind_array=[]
 
-                #Slightly hacky but counts amount of hours that contained rain. 0 hours= none (0),1-2 hours = little(1), 3-4 hours= medium(2), 5-7 hours= lot(3), 8+= massive amount(4)                rain_counter=0
+                #zeroing variables for next location
                 rain_counter=0
                 rain_amount=0
 
                 hour_counter=0
                         
-                        
-        #print(all_data)
-        return( all_data)
+        return(all_data)
 
     #imports the current csv and adds either 1 or 2 more days of data to each location
+    #The "days" parameter determines how many days of data you are adding to each location
     def add_dayData(self, days):
         all_data= self.get_Data(self.key, days)
         currentData=(np.genfromtxt("initial.csv",delimiter=','))
-        #print(currentData[0])
         currentData= currentData.tolist()
+        #keeps track of what position you're on in the array obtained from the get_Data method
         dataCounter=0
         new_data=[]
 
@@ -132,26 +131,33 @@ class Weather_data(object):
         currentData.pop(0)
         currentDays= len(currentData)/self.size
         
-        #print(currentDays)
 
         #for each location, if its the last day of data we have for that location, append the new data to the end of it.
         #the all_data array contains 2 days of data per location, so once you get past the last element per location, add 2 to the counter.
         for element in currentData:
             #print(element[0])
             
-            # if we have 14 days of data per location, get rid of the first two whilst adding to the end.
+            #if we have 14 days of data per location, get rid of the first two whilst adding to the end.
             if currentDays==14:
                 if days==2:
+                    #element[0] contains the index, it tells you which day of data you are on. Does that row show the 5th day of data for that location or the 14th.
+                    #If there's already 14 days of data, simply don't append the 2 oldest days of data to the new array.
                     if element[0]==1 or element[0]==2:
                         pass
                     elif element[0]== currentDays:
+                        #As we've removed the 2 oldest days of data, we must update the indexes of the rest
+                        element[0]=element[0]-1
+                        
                         new_data.append(element)
-                        all_data[dataCounter][0]=currentDays+1
-                        all_data[dataCounter+1][0]=currentDays+2
+                        #Updating the indexes of the 2 next days of data in the array obtained from the get_Data method
+                        all_data[dataCounter][0]=currentDays-1
+                        all_data[dataCounter+1][0]=currentDays
+                        #Appending this to the new array
                         new_data.append(all_data[dataCounter])
                         new_data.append(all_data[dataCounter+1])
                         dataCounter+=days
                     else:
+                        element[0]=element[0]-1
                         new_data.append(element)
 
                 elif days==1:
@@ -159,21 +165,26 @@ class Weather_data(object):
                         pass
                     
                     elif element[0]== currentDays:
+                        #As we're removing he oldest day of data, we must update the indexes of the rest
                         element[0]=element[0]-1
-                        #appended data from day 14, changing index to show its day 13
+                        #Appended data from day 14, changing index to show its day 13
                         new_data.append(element)
                         #Data from the day whose data was just retrieved by the api
                         all_data[dataCounter][0]=currentDays
                         new_data.append(all_data[dataCounter])
                         dataCounter+=1
                     else:
+                        #If there's 14 days of data per location but you aren't currently iterating over the 14th day then just adjust the index and append it to the new array
                         element[0]= element[0]-1
                         print(element[0])
                         new_data.append(element)
-                        
+
+                    #If there's not already 14 days of data per location then just append all the data from the imported csv to the new array   
             else:
                 if element[0]== currentDays:
                     new_data.append(element)
+                    #add the new day of data obtained from the get_Data method to the bottom of each location with the correct index.
+                    #ie: if there's currently 5 days of data per location, add the new day with an index of 6.
                     all_data[dataCounter][0]=currentDays+1
                     new_data.append(all_data[dataCounter])
                     if days==2:
@@ -185,7 +196,7 @@ class Weather_data(object):
 
         return(new_data)
                
-
+    #A method to simplify usage of the class. Can be used to call a method that returns an array and turn that array into a csv.
     def produce_Output(self, which):
 
         if (which)== "initial":
@@ -203,19 +214,18 @@ class Weather_data(object):
         else:
             print("Wrong produce_Output parameters entered")
 
-        #print( all_data[2])
-
         df= pd.DataFrame(all_data,columns=["Days", "Longitude","Latitude", "Temp1", "Hum1","Wind1", "Rain"])
-
+    
         df = df.set_index('Days')
-        #print(df)
+        #Many are called a similar name as they simply add a day onto the main csv and overwrite it
         df.to_csv(name)
 
-    #Will produce a csv that gives past week averages
-    def produce_final(self):
+    #Will produce a csv that gives 14 day averages
+    def produce_average(self):
         currentData=(np.genfromtxt("initial.csv",delimiter=','))
         currentData= currentData.tolist()
 
+        #removes column headings
         currentData.pop(0)
         currentDays= len(currentData)/self.size
         
@@ -225,13 +235,16 @@ class Weather_data(object):
         rainavg=0
         tempArray=[]
 
+        #counts how many empty days of data there are for each location
         zeroedvalues=0
         
         avgArray=[]
         for element in currentData:
+            #if both temperature and humidity are 0 then it's a zeroed day
             if element[4]==0 and element[5]==0:
                 zeroedvalues+=1
             else:
+                #add data to temporary arrays until you reach the end of the data you have for that location
                 humavg+=element[4]
                 temperatureavg+=element[3]
                 windavg+=element[5]
@@ -241,20 +254,22 @@ class Weather_data(object):
                 if zeroedvalues==currentDays:
                     zeroedvalues-=1
                     
+                #temporary arrays are used to calculate averages    
                 humavg=humavg/(currentDays-zeroedvalues)
                 temperatureavg=temperatureavg/(currentDays-zeroedvalues)
                 windavg=round(windavg/(currentDays-zeroedvalues))
                 rainavg=round(rainavg/(currentDays-zeroedvalues))
-                #print(rainavg)
 
-                #tempArray.append(element[1])
-                #tempArray.append(element[2])
+                #all averages are put into one array
                 tempArray.append(temperatureavg)
                 tempArray.append(humavg)
                 tempArray.append(windavg)
                 tempArray.append(rainavg)
 
+                #this one array is nested into a final avgArray - nested arrays are required for conversion to csv.
+                #Each nested array is a row
                 avgArray.append(tempArray)
+                
                 tempArray=[]
                 temperatureavg=0
                 humavg=0
@@ -264,8 +279,7 @@ class Weather_data(object):
         
         df= pd.DataFrame(avgArray,columns=["Tempavg", "Humavg","Windavg", "Rainavg"])
 
-        #print(df)
-        df.to_csv("weather.csv")       
+        df.to_csv("averageWeather.csv")       
 
     def getFutureData(self, key):
 
@@ -285,7 +299,7 @@ class Weather_data(object):
             #Checks weather data per hour, appends to an array then averages the data.
             
             for element in daily_data:
-                #print(count)
+                
                 count+=1
                 
                 if(element['precipIntensity'])<=0.35:
@@ -318,9 +332,9 @@ class Weather_data(object):
 
         return(all_data)
                    
-
+#merges future weather data with past 14 day averages
     def output_to_svm(self):
-        currentData=(np.genfromtxt("weather.csv",delimiter=','))
+        currentData=(np.genfromtxt("averageweather.csv",delimiter=','))
         currentData= currentData.tolist()
         currentData.pop(0)
 
@@ -332,29 +346,28 @@ class Weather_data(object):
         counter=0
         location=0
         for element in futureData:
-            #print(currentData[location])
-            #print(element)
+            #appending averages to the end of the future weather data array
             element.extend(currentData[location][1:5])
-            #print(element)
+            
             finalArray.append(element)
             counter+=1
+            #We have forecasts for the next 6 days of data per location
             if counter==6:
                 location+=1
                 counter=0
 
-        #print(finalArray)
         df= pd.DataFrame(finalArray,columns=["Days", "Longitude","Latitude", "AvgTemp", "AvgHum","AvgWind", "AvgRain", "14dayAvgTemp", "14dayAvgHum", "14dayAvgWind", "14dayAvgRain"])
-        df.to_csv("finalweather.csv")
+        df.to_csv("svmoutput.csv")
 
                 
 file='forest_locations.csv'
-key = 'e5d530b79eb4c2232e41a7ab30a208e4'
+key = ''
 
-#weatherGetter = Weather_data(file,key, 193)
-#weatherGetter.produce_Output("initial")
-#weatherGetter.produce_final()
-#weatherGetter.produce_Output("future")
-#weatherGetter.output_to_svm()
+weatherGetter = Weather_data(file,key, 210)
+weatherGetter.produce_Output("initial")
+weatherGetter.produce_average()
+weatherGetter.produce_Output("future")
+weatherGetter.output_to_svm()
 
 
 
